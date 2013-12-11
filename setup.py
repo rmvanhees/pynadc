382e7171b8164d3af90abf11b8098e81cb8651de
Richard van Hees <r.m.van.hees@sron.nl>
2013-12-11 09:23:54 +0100

    [pynadc] added script collect_stateDefs.py to setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@
 from distutils.core import setup
 
 setup( name='pynadc',
-       version='1.0',
+       version='1.1',
        description='Python Sciamachy read and sqlite3 library',
        author='Richard van Hees',
        author_email='rm.vanhees@gmail.com',
        packages=['scia'],
        package_dir={'scia': 'src/scia'},
        scripts=['scripts/inquire_db2.py', 'scripts/sdmf_calibSMR.py',
-                'scripts/scia_lv0.py', 'scripts/scia_lv1.py']
+                'scripts/scia_lv0.py', 'scripts/scia_lv1.py',
+                'scripts/collect_stateDefs.py']
        )
