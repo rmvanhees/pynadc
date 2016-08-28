e534b94b51dc60a8327eab06f2f9799ee9733cc4
RM. van Hees <rm.vanhees@gmail.com>
2016-08-28 10:23:25 +0200

    icm_plot: introduced class ICM_plot

--- sources/pynadc/tropomi/icm_plot.py
+++ sources/pynadc/tropomi/icm_plot.py
@@ -8,6 +8,7 @@ import numpy as np
 import h5py
 
 import matplotlib as mpl
+mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 
 from matplotlib import gridspec
@@ -16,89 +17,115 @@ from matplotlib.backends.backend_pdf import PdfPages
 from pynadc import extendedrainbow_with_outliers
 from pynadc.tropomi.icm_mon_db import ICM_mon
 
-cmap = "Rainbow"
+class ICM_plot(object):
+    def __init__( self, dbname, res_sql, cmap="Rainbow" ):
+        self.__algo_version = res_sql['algVersion'][0]
+        self.__db_version   = res_sql['dbVersion'][0]
+        self.__icm_version  = res_sql['icmVersion'][0]
+        date = res_sql['startDateTime'][0][0:10].replace('-','')
+        orbit = res_sql['referenceOrbit'][0]
+        self.__pdf = PdfPages( dbname + '_{}_{}.pdf'.format(date, orbit) )
+        self.__cmap = cmap
+        
+    def __repr__( self ):
+        pass
 
-def plot_icm_info( fig, mon ):
-    '''
-    '''
-    from datetime import datetime
+    def __del__( self ):
+        self.__pdf.close()
 
-    cre_date = datetime.utcnow().isoformat(' ')[0:19]
+    def __icm_info( self, fig ):
+        '''
+        '''
+        from datetime import datetime
+
+        cre_date = datetime.utcnow().isoformat(' ')[0:19]
     
-    info_str = 'date : {}'.format(cre_date) \
-               + '\nicm_version  : {}'.format(mon.h5_get_attr('icm_version')) \
-               + '\nalgo_version : {}'.format(mon.h5_get_attr('algo_version')) \
-               + '\ndb_version   : {}'.format(mon.h5_get_attr('db_version'))
+        info_str = 'date : {}'.format(cre_date) \
+                   + '\nicm_version  : {}'.format(self.__icm_version) \
+                   + '\nalgo_version : {}'.format(self.__algo_version) \
+                   + '\ndb_version   : {}'.format(self.__db_version)
     
-    fig.text( 0.025, 0.225, info_str,
-              verticalalignment='top', horizontalalignment='left',
-              bbox={'facecolor':'white', 'pad':10},
-              fontsize=10, style='italic' )
-    fig.text( 0.025, 0.875,
-              r'$\copyright$ {}'.format(mon.h5_get_attr('institute')) )
-    
-    
-def plot_icm_data( pdf, mon, signal, signal_col, signal_row ):
-    '''
-    '''
-    fig = plt.figure(figsize=(24,10.5)) 
-    fig.suptitle( mon.h5_get_attr('title' ), fontsize=24 )
-    gs = gridspec.GridSpec(6,16) 
+        fig.text( 0.015, 0.075, info_str,
+                  verticalalignment='bottom', horizontalalignment='left',
+                  bbox={'facecolor':'white', 'pad':10},
+                  fontsize=10, style='italic' )
+        fig.text( 0.025, 0.875,
+                  r'$\copyright$ SRON, Netherlands Institute for Space Research' )
 
-    ax0 = plt.subplot(gs[1:4,0:2])
-    ax0.plot(signal_row, np.arange(signal_row.size))
-    ax0.set_ylim( [0, signal_row.size-1] )
-    ax0.locator_params(axis='x', nbins=3)
-    ax0.set_xlabel( 'signal', fontsize=18 )
-    ax0.set_ylabel( 'row', fontsize=18 )
+    def __frame( self, data_label, mon, signal, signal_col, signal_row ):
+        '''
+        '''
+        fig = plt.figure(figsize=(24,10.5)) 
+        fig.suptitle( mon.h5_get_attr('title' ), fontsize=24 )
+        gs = gridspec.GridSpec(6,16) 
 
-    ax1 = plt.subplot(gs[1:4,2:14])
-    ax1.imshow( signal, cmap=cmap, aspect=1,
-                interpolation='none', origin='lower' )
-    ax1.set_title( mon.h5_get_attr('comment' ) )
+        ax0 = plt.subplot(gs[1:4,0:2])
+        ax0.plot(signal_col, np.arange(signal_col.size))
+        ax0.set_xlabel( data_label, fontsize=18 )
+        ax0.set_ylim( [0, signal_col.size-1] )
+        ax0.locator_params(axis='x', nbins=3)
+        ax0.set_ylabel( 'row', fontsize=18 )
 
-    ax3 = plt.subplot(gs[4:6,2:14])
-    ax3.plot(np.arange(signal_col.size), signal_col)
-    ax3.set_ylabel( 'signal', fontsize=18 )
-    ax3.set_xlabel( 'column', fontsize=18 )
+        ax1 = plt.subplot(gs[1:4,2:14])
+        (p_05, p_95) = np.percentile( signal, (5,95) )        
+        ax1.imshow( signal, cmap=self.__cmap, aspect=1, vmin=p_05, vmax=p_95,
+                    interpolation='none', origin='lower' )
+        ax1.set_title( mon.h5_get_attr('comment' ) )
 
-    ax4 = plt.subplot(gs[1:6,14])
-    norm = mpl.colors.Normalize(vmin=-1, vmax=1)
-    cb1 = mpl.colorbar.ColorbarBase( ax4, cmap=cmap, norm=norm,
-                                     orientation='vertical' )
-    cb1.set_label( 'electron / s', fontsize=18 )
+        ax3 = plt.subplot(gs[4:6,2:14])
+        ax3.plot(np.arange(signal_row.size), signal_row)
+        ax3.set_ylabel( data_label, fontsize=18 )
+        ax3.set_xlim( [0, signal_row.size-1] )
+        ax3.set_xlabel( 'column', fontsize=18 )
 
-    plot_icm_info( fig, mon )
-    plt.tight_layout()
-    pdf.savefig()
-    plt.close()
+        ax4 = plt.subplot(gs[1:6,14])
+        norm = mpl.colors.Normalize(vmin=p_05, vmax=p_95)
+        cb1 = mpl.colorbar.ColorbarBase( ax4, cmap=self.__cmap, norm=norm,
+                                         orientation='vertical' )
+        cb1.set_label( 'electron / s', fontsize=18 )
 
-def plot_icm_hist():
-    '''
-    '''
-    fig = plt.figure(figsize=(24,10.5))
-    gs = gridspec.GridSpec(6,16) 
+        self.__icm_info( fig )
+        plt.tight_layout()
+        self.__pdf.savefig()
+        plt.close()
 
-    ax0 = plt.subplot(gs[1:4,0:8])
-    ax0.hist( signal.reshape(-1), 20 )
-    ax0.set_ylabel( 'count', fontsize=18 )
-    ax0.set_xlabel( 'signal', fontsize=18 )
 
-    ax1 = plt.subplot(gs[1:4,8:16])
-    ax1.hist( signal_std.reshape(-1), 20 )
-    ax1.set_ylabel( 'count', fontsize=18 )
-    ax1.set_xlabel( 'error', fontsize=18 )
+    def draw_signal( self, mon, signal, signal_col, signal_row ):
+        '''
+        '''
+        self.__frame( 'signal', mon, signal, signal_col, signal_row )
 
-    ax3 = plt.subplot(gs[4:6,2:16])
-    ax3.plot(15 * np.arange(signal_col_std.size), signal_col_std)
-    ax3.set_xlim( [0, np.max(15 * np.arange(signal_col_std.size))] )
-    ax3.set_ylabel( 'average signal', fontsize=18 )
-    ax3.set_xlabel( 'orbit', fontsize=18 )
+    def draw_errors( self, mon, signal, signal_col, signal_row ):
+        '''
+        '''
+        self.__frame( 'errors', mon, signal, signal_col, signal_row )
 
-    fig.text( 0.025, 0.875, r'$\copyright$ {}'.format(institute) )
-    plt.tight_layout()
-    pdf.savefig()
-    plt.close()
+    def __hist( self ):
+        '''
+        '''
+        fig = plt.figure(figsize=(24,10.5))
+        gs = gridspec.GridSpec(6,16) 
+
+        ax0 = plt.subplot(gs[1:4,0:8])
+        ax0.hist( signal.reshape(-1), 20 )
+        ax0.set_ylabel( 'count', fontsize=18 )
+        ax0.set_xlabel( 'signal', fontsize=18 )
+
+        ax1 = plt.subplot(gs[1:4,8:16])
+        ax1.hist( signal_std.reshape(-1), 20 )
+        ax1.set_ylabel( 'count', fontsize=18 )
+        ax1.set_xlabel( 'error', fontsize=18 )
+
+        ax3 = plt.subplot(gs[4:6,2:16])
+        ax3.plot(15 * np.arange(signal_col_std.size), signal_col_std)
+        ax3.set_xlim( [0, np.max(15 * np.arange(signal_col_std.size))] )
+        ax3.set_ylabel( 'average signal', fontsize=18 )
+        ax3.set_xlabel( 'orbit', fontsize=18 )
+
+        fig.text( 0.025, 0.875, r'$\copyright$ {}'.format(institute) )
+        plt.tight_layout()
+        pdf.savefig()
+        plt.close()
     
 ## read data of latest entry
 def test():
@@ -108,14 +135,20 @@ def test():
     print( orbit )
     res_sql = mon.sql_select_orbit( orbit, full=True )
     print( res_sql )
-    res_h5 = mon.h5_read_frames( res_sql['rowID'][0], statistics='rows,cols' )
+    res_h5 = mon.h5_read_frames( res_sql['rowID'][0],
+                                 statistics='error,rows,cols' )
     print( res_h5.keys() )
+    for name in res_h5.keys():
+        print( name, res_h5[name].shape )
 
     ## plot latest entry
-    with PdfPages( DBNAME + '_{}.pdf'.format(orbit) ) as pdf:
-        plot_icm_data( pdf, mon, res_h5['signal'],
-                       res_h5['signal_col'], res_h5['signal_row'] )
-
+    plot = ICM_plot( DBNAME, res_sql )
+    plot.draw_signal( mon, res_h5['signal'],
+                      res_h5['signal_col'], res_h5['signal_row'] )
+    plot.draw_errors( mon, res_h5['signal_std'],
+                      res_h5['signal_col_std'], res_h5['signal_row_std'] )
+    del(mon)
+    del(plot)
     ## read data of penultimate entry
     ## plot difference with penultimate result
     ## read data for time-series
